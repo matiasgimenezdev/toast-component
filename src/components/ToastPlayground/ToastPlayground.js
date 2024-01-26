cbffd84f7caa9d2ce0bbbd3f9b7b94448fc865ce
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-01-26 20:08:18 -0300

    refactoring context

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,98 +2,92 @@ import React, { useContext, useState } from 'react';
 import Button from '../Button';
 import styles from './ToastPlayground.module.css';
 import ToastShelf from '../ToastShelf';
-import ToastProvider, { ToastContext } from '../ToastProvider/ToastProvider';
+import { ToastContext } from '../ToastProvider';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
 	const [message, setMessage] = useState('');
 	const [variant, setVariant] = useState(VARIANT_OPTIONS[0]);
-	const [toasts, setToasts] = useContext(ToastContext);
+	const { createToast, dismissToast, dismissAllToasts } =
+		useContext(ToastContext);
 
 	function handleSubmit(event) {
 		event.preventDefault();
-		const id = crypto.randomUUID();
-		const nextToasts = [...toasts, { id, message, variant }];
-		setToasts(nextToasts);
+		createToast(message, variant);
 		setMessage('');
 		setVariant(VARIANT_OPTIONS[0]);
 	}
 
-	function handleDismiss(id) {
-		const nextToasts = toasts.filter((toast) => toast.id !== id);
-		setToasts(nextToasts);
-	}
-
 	return (
-		<ToastProvider>
-			<div className={styles.wrapper}>
-				<header>
-					<img alt='Cute toast mascot' src='/toast.png' />
-					<h1>Toast Playground</h1>
-				</header>
-				<ToastShelf toasts={toasts} handleDismiss={handleDismiss} />
-				<form
-					onSubmit={handleSubmit}
-					className={styles.controlsWrapper}
-				>
-					<div className={styles.row}>
-						<label
-							htmlFor='message'
-							className={styles.label}
-							style={{ alignSelf: 'baseline' }}
-						>
-							Message
-						</label>
-						<div className={styles.inputWrapper}>
-							<textarea
-								id='message'
-								className={styles.messageInput}
-								value={message}
-								onChange={(event) => {
-									setMessage(event.target.value);
-								}}
-							/>
-						</div>
+		<div className={styles.wrapper}>
+			<header>
+				<img alt='Cute toast mascot' src='/toast.png' />
+				<h1>Toast Playground</h1>
+			</header>
+			<ToastShelf
+				handleDismiss={(id) => dismissToast(id)}
+				handleDismissAll={() => {
+					dismissAllToasts();
+				}}
+			/>
+			<form onSubmit={handleSubmit} className={styles.controlsWrapper}>
+				<div className={styles.row}>
+					<label
+						htmlFor='message'
+						className={styles.label}
+						style={{ alignSelf: 'baseline' }}
+					>
+						Message
+					</label>
+					<div className={styles.inputWrapper}>
+						<textarea
+							id='message'
+							className={styles.messageInput}
+							value={message}
+							onChange={(event) => {
+								setMessage(event.target.value);
+							}}
+						/>
 					</div>
+				</div>
 
-					<div className={styles.row}>
-						<div className={styles.label}>Variant</div>
-						<div
-							className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-						>
-							{VARIANT_OPTIONS.map((option) => {
-								const id = `variant-${option}`;
-								return (
-									<label htmlFor={id} key={id}>
-										<input
-											id={id}
-											type='radio'
-											name='variant'
-											value={option}
-											onChange={(event) => {
-												setVariant(event.target.value);
-											}}
-											checked={variant === option}
-										/>
-										{option}
-									</label>
-								);
-							})}
-						</div>
+				<div className={styles.row}>
+					<div className={styles.label}>Variant</div>
+					<div
+						className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+					>
+						{VARIANT_OPTIONS.map((option) => {
+							const id = `variant-${option}`;
+							return (
+								<label htmlFor={id} key={id}>
+									<input
+										id={id}
+										type='radio'
+										name='variant'
+										value={option}
+										onChange={(event) => {
+											setVariant(event.target.value);
+										}}
+										checked={variant === option}
+									/>
+									{option}
+								</label>
+							);
+						})}
 					</div>
+				</div>
 
-					<div className={styles.row}>
-						<div className={styles.label} />
-						<div
-							className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-						>
-							<Button type='submit'>Pop Toast!</Button>
-						</div>
+				<div className={styles.row}>
+					<div className={styles.label} />
+					<div
+						className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+					>
+						<Button type='submit'>Pop Toast!</Button>
 					</div>
-				</form>
-			</div>
-		</ToastProvider>
+				</div>
+			</form>
+		</div>
 	);
 }
 
--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,13 +1,34 @@
-import { createContext, useMemo, useState } from 'react';
+import { createContext, useCallback, useMemo, useState } from 'react';
 
 export const ToastContext = createContext();
 
 function ToastProvider({ children }) {
 	const [toasts, setToasts] = useState([]);
 
+	const createToast = useCallback(
+		(message, variant) => {
+			const id = crypto.randomUUID();
+			const nextToasts = [...toasts, { id, message, variant }];
+			setToasts(nextToasts);
+		},
+		[toasts]
+	);
+
+	const dismissToast = useCallback(
+		function dismissToast(id) {
+			const nextToasts = toasts.filter((toast) => toast.id !== id);
+			setToasts(nextToasts);
+		},
+		[toasts]
+	);
+
+	function dismissAllToasts() {
+		setToasts([]);
+	}
+
 	const value = useMemo(() => {
-		return [toasts, setToasts];
-	}, [toasts]);
+		return { toasts, createToast, dismissToast, dismissAllToasts };
+	}, [toasts, createToast, dismissToast]);
 
 	return (
 		<ToastContext.Provider value={value}>{children}</ToastContext.Provider>
